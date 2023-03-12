2738d94b1e9b0a1fc67516d997f8c2e8dfab4e04
Narasimman2067 <narasimmamech2067@gmail.com>
2023-03-13 03:10:46 +0530

    cv

--- src/Components/AddStudent.js
+++ src/Components/AddStudent.js
@@ -8,15 +8,15 @@ export function AddStudent({employeesData,setEmployees}) {
    const history=useHistory();
 
    useEffect(()=>{
-    if(!localStorage.getItem("user-"))
+    if(!localStorage.getItem("user-name"))
     history.replace("/login")
     },[])
   
    
-    const [product_name, setProductName] = useState("");
-    const [product_price, setProductPrice] = useState("");
-    const [product_material, setProductMaterial] = useState("");
-    const [product_color, setProductColor] = useState("");
+    const [name, setName] = useState("");
+    const [batch, setBatch] = useState("");
+    const [gender, setGender] = useState("");
+    const [Experiences, setExperience] = useState("");
    
    
     
@@ -24,10 +24,10 @@ export function AddStudent({employeesData,setEmployees}) {
 event.preventDefault();
       try {
         const newEmployee = {
-          product_name,
-          product_price,
-          product_material,
-          product_color,
+          name,
+          batch,
+          gender,
+          yearsOfExperience: Experiences,
         };
 // after we create the new data use fetch function here
 const response=await fetch("https://63ae58f1ceaabafcf177e2a6.mockapi.io/data",
@@ -45,10 +45,10 @@ console.log(data);
         setEmployees([...employeesData, data]);
         // after add function done  immediately it refresh the input field
        
-        setProductName("");
-        setProductPrice("");
-        setProductMaterial("");
-        setProductColor("");
+        setName("");
+        setBatch("");
+        setGender("");
+        setExperience("");
         history.push("/user")
         
       } catch (error) {
@@ -63,42 +63,42 @@ console.log(data);
 <div>
 
    <Base>
-  <h1 class='head'>you can add a student</h1>
-     <div class="input-div">
+  <h1 className='head'>you can add a student</h1>
+     <div className="input-div">
      
            
   
         <TextField
          required
           id="outlined-required"
-          label="Enter your "
+          label="Enter your name"
           variant="outlined"
-          onChange={(event) => setProductName(event.target.value)}
-          value={product_name}
+          onChange={(event) => setName(event.target.value)}
+          value={name}
         />
         <TextField
          required
           id="outlined-basic"
-          label="Enter your product_price"
+          label="Enter your batch"
           variant="outlined"
-          onChange={(event) => setProductPrice(event.target.value)}
-          value={product_price}
+          onChange={(event) => setBatch(event.target.value)}
+          value={batch}
         />
         <TextField
           required="text"
           id="outlined-basic"
-          label="Enter your product_material"
+          label="Enter your gender"
           variant="outlined"
-          onChange={(event) => setProductMaterial(event.target.value)}
-          value={product_material}
+          onChange={(event) => setGender(event.target.value)}
+          value={gender}
         />
         <TextField
         required={Number}
           id="outlined-basic"
-          label="Enter your "
+          label="Enter your experiences"
           variant="outlined"
-          onChange={(event) => setProductColor(event.target.value)}
-          value={product_color}
+          onChange={(event) => setExperience(event.target.value)}
+          value={Experiences}
         />
       </div>
         <Button
