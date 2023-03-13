4e4d759429c418ab31a773c745e2638e81b8497f
Narasimman2067 <narasimmamech2067@gmail.com>
2023-03-13 12:48:21 +0530

    c

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,25 +1,37 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
-const path = require('path');
+const path =require ('path')
 
-module.exports = {
-  output: {
-    filename: 'my-first-webpack.bundle.js',
-    publicPath: "/"
-  },
-  module: {
-    rules: [{ test: /\.txt$/, 
-    use: 'raw-loader' }],
-  },
-};
-module.exports = {
-  module: {
-    rules: [
-      { test: /\.txt$/,
-       use: 'raw-loader' }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
-};
+module.exports={
+    outputs:{
+        path:path.join(_dirname,'/dist'),
+        filename:'index.js',
+
+    },
+    devServer:{
+        port:3000,
+        watchContentBase:false,
+    },
+    module:
+    {
+            rules:[{
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+
+
+            },
+        {
+            test:/\.scss$/,
+            use:[
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+                'file-loader'
+
+            ]
+        }
+        ]
+    }
+
+}
